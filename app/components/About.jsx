7468a64526103a22b7dc39b0783bd0a4ba54e428
Peter Martinez <Peter Martinez>
2017-04-12 22:10:24 -0600

    Styling the Main Component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,18 +15,18 @@ var About = (props) => {
     <p className='text-justify'>This is single page React Application will tell you the tempature in your area, currently this app is using the Open Weather Map API which you can find in the resources below,  you will also find the source code that is used in the app below as well.</p>
     <h3 className="text-center">Resources</h3>
     <table>
-
-        <tr>
-        <td>Open Weather Map:</td>
-        <td> <a href='http://openweathermap.org/'>Click Here</a></td>
+    <tr>
+      <td>Open Weather Map:</td>
+      <td> <a href='http://openweathermap.org/'>Click Here</a></td>
     </tr>
     <tr>
       <td>Github Repo:</td>
       <td> <a href='https://github.com/NeonKraze/ReactWeather'>Click Here</a></td>
     </tr>
-
-
-
+    <tr>
+      <td>React:</td>
+      <td> <a href='https://facebook.github.io/react/'>Click Here</a></td>
+    </tr>
   </table>
     </div>
   );
