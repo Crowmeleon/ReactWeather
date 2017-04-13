035aecdac8927c047711533b77114652df34a605
Peter Martinez <Peter Martinez>
2017-04-12 23:27:34 -0600

    Added in Error Modal for invalid City

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
@@ -11,7 +12,11 @@ var Weather = React.createClass({
   },
   handleSearch: function(location){
     var that = this;
-    this.setState({isLoading: true});
+
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
@@ -19,26 +24,29 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false
       });
-    },
-  function(errorMessage){
-    alert(errorMessage);
+    }, function(e){
     that.setState({
-      isLoading: false
+      isLoading: false,
+      errorMessage: e.message
     });
   });
   },
   render: function(){
-    var temp = this.state.temp;
-    var isLoading = this.state.isLoading;
-    var location = this.state.location;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage () {
       if(isLoading){
         return <h3 className="text-center">Fetching weather</h3>;
       }else if (temp && location) {
         return <WeatherMessage temp={temp} location={location} />;
-      }else {
+      }
+    }
 
+    function renderError (){
+      if(typeof errorMessage === 'string'){
+        return(
+          <ErrorModal message={errorMessage}/>
+        );
       }
     }
 
@@ -47,6 +55,7 @@ var Weather = React.createClass({
       <h1 className="text-center">Get Weather</h1>
       <WeatherForm onSearch={this.handleSearch}/>
       {renderMessage()}
+      {renderError()}
       </div>
     );
   }
