caf94fb8d230d07f10a34661c54f445e6fbd7c23
Peter Martinez <Peter Martinez>
2017-04-12 21:19:16 -0600

    Add Foundation and jQuery

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-    <h3>About Component</h3>
+    <h3>About</h3>
     <p>This single page React Application will tell you the tempature in your area</p>
     </div>
   );
