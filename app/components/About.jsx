713091d0bf6f664137a44ed72bbe7fbf096c678c
Peter Martinez <Peter Martinez>
2017-04-12 22:01:05 -0600

    Styled the About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,23 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-    <h3>About</h3>
-    <p>This single page React Application will tell you the tempature in your area</p>
+    <h1 className='text-center'>About</h1>
+    <p className='text-justify'>This is single page React Application will tell you the tempature in your area, currently this app is using the Open Weather Map API which you can find in the resources below,  you will also find the source code that is used in the app below as well.</p>
+    <h3 className="text-center">Resources</h3>
+    <table>
+
+        <tr>
+        <td>Open Weather Map:</td>
+        <td> <a href='http://openweathermap.org/'>Click Here</a></td>
+    </tr>
+    <tr>
+      <td>Github Repo:</td>
+      <td> <a href='https://github.com/NeonKraze/ReactWeather'>Click Here</a></td>
+    </tr>
+
+
+
+  </table>
     </div>
   );
 };
