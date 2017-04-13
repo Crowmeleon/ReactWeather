5f9fea401b1c88956f4a4a984635dfab506ae6e3
Peter Martinez <Peter Martinez>
2017-04-12 19:59:02 -0600

    This is an Update to the Examplesjsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,7 +13,8 @@ var React = require('react');
 var Examples = (props) => {
   return(
     <div>
-      <h3>This is the Examples Page</h3>
+      <h3>This is the Examples Page!</h3>
+      <p>Welcome to the Examples Page</p>
     </div>
   );
 };
